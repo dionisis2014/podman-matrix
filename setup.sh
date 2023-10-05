8377d712c4025de03d254935ffcd3e1682c64b6d
dionisis2014 <toulatosdionisis@gmail.com>
2023-10-06 01:29:27 +0300

    Check if container exists before polling status

--- setup.sh
+++ setup.sh
@@ -6,11 +6,11 @@ source config.sh	# load config variables
 # Admin account setup
 echo -e '\e[32mSetting up administrator account ...\e[0m'
 systemctl --user start container-matrix-dendrite.service
-until [ $(podman container inspect -f '{{.State.Health.Status}}' matrix-postgres) == 'healthy' ]
+until [ $(podman container exists matrix-postgres && podman container inspect -f '{{.State.Health.Status}}' matrix-postgres) == 'healthy' ]
 do
 	sleep 0.1
 done
-until [ $(podman container inspect -f '{{.State.Running}}' matrix-dendrite) == 'true' ]
+until [ $(podman container exists matrix-dendrite && podman container inspect -f '{{.State.Running}}' matrix-dendrite) == 'true' ]
 do
 	sleep 0.1
 done
