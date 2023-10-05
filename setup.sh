1d6b736b05412ef5c5f0f160d9fe670a8b39654f
dionisis2014 <toulatosdionisis@gmail.com>
2023-10-06 02:28:11 +0300

    Add quotes to until blocks

--- setup.sh
+++ setup.sh
@@ -6,13 +6,13 @@ source config.sh	# load config variables
 # Admin account setup
 echo -e '\e[32mSetting up administrator account ...\e[0m'
 systemctl --user start container-matrix-dendrite.service
-until [ $(podman container exists matrix-postgres && podman container inspect -f '{{.State.Health.Status}}' matrix-postgres) == 'healthy' ]
+until [ "$(podman container exists matrix-postgres && podman container inspect -f '{{.State.Health.Status}}' matrix-postgres)" == 'healthy' ]
 do
-	sleep 0.1
+	sleep 0.5
 done
-until [ $(podman container exists matrix-dendrite && podman container inspect -f '{{.State.Running}}' matrix-dendrite) == 'true' ]
+until [ "$(podman container exists matrix-dendrite && podman container inspect -f '{{.State.Running}}' matrix-dendrite)" == 'true' ]
 do
-	sleep 0.1
+	sleep 0.5
 done
 echo "Enter the administrator user's password when prompted"
 podman container exec -ti matrix-dendrite /usr/bin/create-account --config /etc/dendrite/dendrite.yaml --username "${DENDRITE_ADMIN}"
